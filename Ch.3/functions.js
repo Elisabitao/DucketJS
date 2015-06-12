60ddc0706084b3c0621215121839ffb82549b96e
simacna <simacna@gmail.com>
2015-06-11 20:09:56 -0400

    pushing all unsaved data before leaving

--- Ch.3/functions.js
+++ Ch.3/functions.js
@@ -134,6 +134,17 @@ shape.getWidth(); //this now returns 300
 
 
 
+//working with decimal numbers
+
+var originalNumber = 10.23456;
+
+var msg = '<h2> origin number</h2><p>' + originalNumber + '</p>';
+msg += "<h2> 3 decimal places </h2><p>" + originalNumber.toFixed(3); + '</p>';
+msg += "<h2> 3 digits</h2><p>" + originalNumber.toPrecision(3) + '</p>;
+
+var el = document.getElementById("info");
+el.innerHTML = msg; 
+
 
 
 
