fac0f2e02393a4b88ed2fd21985793b0961da5fc
simacna <simacna@gmail.com>
2015-06-24 17:34:15 -0400

    starting ch. 4

---
+++ Ch.4/loops.js
@@ -0,0 +1,13 @@
+// 6/24/15
+
+// Because the presence of an object or array can be considered truthy,it is often used to check for the existence of an 
+// elemlent within a page. 
+// as soon as a truthy value is found, the remaining options are not checked. Experienced programmers often:
+// 1. Put the code most likely to return true first in OR operations and false answers first in an AND operation
+
+// Keywords often seen in loops (and sometimes functions):
+// 1. break - this Keyword causes the termination of the loop and tells the interpreter to go onto the next statement of code
+// outside of the loop
+// 2. continue - this keyword tells the interpreter to stop the current iteration and then check the condition again (if its
+// 	true, the code runs again)
+
